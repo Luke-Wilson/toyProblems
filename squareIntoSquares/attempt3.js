c2dc0801ad85a6fa21bc795150cb54f268a6831c
Luke Wilson <wilson.palooza@gmail.com>
2017-03-06 07:27:10 -0800

    improve so that all tests pass, but still too slow

--- squareIntoSquares/attempt3.js
+++ squareIntoSquares/attempt3.js
@@ -1,24 +1,23 @@
-var getAnswer = function(possibles, target) {
+var getAnswer = function(possibles, target, answer = []) {
   //base case
   var remaining = target;
-  if (possibles.length === 0) {
-    return null;
-  }
-  var answer = [];
-  possibles.forEach(possible => {
-    if (possible <= remaining) {
-      remaining -= possible;
-      answer.unshift(Math.sqrt(possible));
-    }
-  });
   if (remaining === 0) {
     return answer;
   }
 
-  if (remaining > 0) {
-    return getAnswer(possibles.slice(1), target);
+  for (var i = 0; i < possibles.length; i++) {
+    if (possibles[i] <= remaining) {
+      remaining -= possibles[i];
+      answer.unshift(possibles[i]);
+      var potentialAnswer = getAnswer(possibles.slice(i+1), remaining, answer);
+      if (potentialAnswer !== null) {
+        return potentialAnswer;
+      }
+      answer.shift();
+      remaining += possibles[i];
+    }
   }
-  return 'something may have gone wrong';
+  return null;
 };
 
 var decompose = function(n) {
@@ -26,9 +25,18 @@ var decompose = function(n) {
   for (var i = 1; i < n; i++ ) {
     possibles.unshift(i * i);
   }
-  return getAnswer(possibles, n*n);
-}
+  var result = getAnswer(possibles, n*n);
+  if (!Array.isArray(result)) {
+    return null;
+  } else {
+    return result.map(ele => Math.sqrt(ele));
+  }
+};
 
-console.log(decompose(50))  //Expected: '[1, 3, 5, 8, 49]', instead got: '[14, 48]'
+var squares11 = [100,81,64,49,36,25,16,9,4,1];
+
+// console.log(getAnswer(squares11, 121));
+console.log(decompose(11))
+// console.log(decompose(50))  //Expected: '[1, 3, 5, 8, 49]', instead got: '[14, 48]'
 
 
